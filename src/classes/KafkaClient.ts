e175d6cb0c36aa5306e052272754390358717ee2
CameronJYoung <cameronjyoung123@gmail.com>
2022-01-23 22:32:51 +0000

    completed consumer and producer methods to allow for full kafka communication

--- src/classes/KafkaClient.ts
+++ src/classes/KafkaClient.ts
@@ -1,23 +1,32 @@
-import { Kafka, Producer } from 'kafkajs';
+import { Consumer, Kafka, Producer, EachMessagePayload } from 'kafkajs';
 
 import pino from '../singletons/Logger';
 import { jsonToBuffer } from '../utils/messageConversions';
+import { MessageHandler } from '../utils/eachMessageHandler';
+
+export interface ITopic {
+	topic: string;
+	handler: MessageHandler
+}
 
 export default class KafkaClient {
 	private clientId: string;
 	private brokers: Array<string>;
 	private kafkaInstance: Kafka;
-	private producer: Producer
+	private producer: Producer;
+	private consumer: Consumer;
 
-	constructor(clientId: string, brokers: Array<string>) {
+	constructor(clientId: string, brokers: Array<string>, groupId: string) {
 		this.clientId = clientId;
 		this.brokers = brokers;
 
 		this.kafkaInstance = new Kafka({
 			clientId: this.clientId,
-			brokers: this.brokers
+			brokers: this.brokers,
 		});
+
 		this.producer = this.kafkaInstance.producer();
+		this.consumer = this.kafkaInstance.consumer({ groupId: groupId });
 	}
 
 	public async produceMessage(message: Record<string, unknown>) {
@@ -37,5 +46,21 @@ export default class KafkaClient {
 			console.log(err);
 			return;
 		});
+	}
+
+	public async listenToTopics(topics: ITopic[]) { // INSERT INTERFACE HERE CONTAINING TOPICS AND HANDLER MAYBE INPUT?
+		await this.consumer.connect();
+
+		topics.forEach(async (i) => {
+			await this.consumer.subscribe({topic: i.topic});
+
+			await this.consumer.run({
+				eachMessage: async (message) => {
+					i.handler(message, i.topic);
+				}
+			});
+		});
+
+
 	}
 };
