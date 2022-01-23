e7d5369031ce6b89af28b0acf98a9a520f14224e
CameronJYoung <cameronjyoung123@gmail.com>
2022-01-23 22:33:33 +0000

    updated imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import KafkaClient from './classes/KafkaClient';
+import KafkaClient, { ITopic } from './classes/KafkaClient';
 import KafkaClientFactory from './factories/KafkaClientFactory';
+import { MessageHandler } from './utils/eachMessageHandler'
 import * as messageConversions from './utils/messageConversions';
 
 export {
 	KafkaClient,
 	KafkaClientFactory,
+	MessageHandler,
+	ITopic,
 	messageConversions
 };
