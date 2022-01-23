989e4db5104d31c1472f2bb6a93cc60ef0abd5f0
CameronJYoung <cameronjyoung123@gmail.com>
2022-01-23 22:33:12 +0000

    added groupid to factory

--- src/factories/KafkaClientFactory.ts
+++ src/factories/KafkaClientFactory.ts
@@ -1,7 +1,7 @@
 import KafkaClient from '../classes/KafkaClient';
 
 export default class KafkaClientFactory {
-	static getKafkaClient(clientId: string, brokers: Array<string>): KafkaClient {
-		return new KafkaClient(clientId, brokers);
+	static getKafkaClient(clientId: string, brokers: Array<string>, groupId: string): KafkaClient {
+		return new KafkaClient(clientId, brokers, groupId);
 	}
 }
