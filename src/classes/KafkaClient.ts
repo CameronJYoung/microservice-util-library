191690af1aa9d6c249edd82ab6baa6a05f5024d0
CameronJYoung <cameronjyoung123@gmail.com>
2022-01-25 21:21:05 +0000

    added log for debugging

--- src/classes/KafkaClient.ts
+++ src/classes/KafkaClient.ts
@@ -45,6 +45,7 @@ export default class KafkaClient {
 
 	public async listenToTopics(topics: ITopic[]) {
 		await this.consumer.connect();
+		console.log(topics);
 		
 		topics.map(async (i) => {
 			await this.consumer.subscribe({topic: i.topic});
