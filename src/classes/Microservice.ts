7616f6f5dccff55d756cf720e33e9c251b76d56c
CameronJYoung <cameronjyoung123@gmail.com>
2022-02-06 21:06:50 +0000

    completed basic Microservice class

--- src/classes/Microservice.ts
+++ src/classes/Microservice.ts
@@ -1,11 +1,20 @@
 import BaseController from "./BaseController";
 import ITopic from "../@types/ITopic";
+import KafkaClient from "./KafkaClient";
 
 export default class Microservice {
+	public microserviceName: string;
 	private controllers: BaseController[];
+	private kafkaClient: KafkaClient;
+	private microserviceTopics: ITopic[];
+	private adminTopic: string|null;
 
-	constructor(microserviceName: string, controllers: BaseController[]) {
+	constructor(microserviceName: string, controllers: BaseController[], kafkaClient: KafkaClient, adminTopic: string|null) {
+		this.microserviceName = microserviceName;
 		this.controllers = controllers;
+		this.kafkaClient = kafkaClient;
+		this.microserviceTopics = this.aggregateTopics();
+		this.adminTopic = adminTopic;
 	}
 
 	private aggregateTopics() {
@@ -20,9 +29,14 @@ export default class Microservice {
 
 
 
-
-
-	public run() {
-
+	public async run() {
+		if (this.adminTopic) {
+			this.kafkaClient.produceMessage({
+				microserviceName: this.microserviceName,
+				topics: this.microserviceTopics
+			}, `${this.adminTopic}.registerService`)
+			
+		}
+		await this.kafkaClient.listenToTopics(this.microserviceTopics);
 	}
 }
